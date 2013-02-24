681e3e307fb81b4030c9b933e8842309f9317f09
Christoph Baudson <christoph@baudson.de>
2013-02-24 14:02:45 +0100

    experimenting with jshint

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 jshint js/mobipick.js --config tests/jshint.json
+if [ "$?" != "0" ]; then
+	exit 1;
+fi
 phantomjs tests/runner.js tests/tests.html
+if [ "$?" != "0" ]; then
+	exit 2;
+fi
+
